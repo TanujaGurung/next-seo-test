4a6efcbfc2a3a20bdb699260574364f9ea04d29d
TanuAtSoft <tanuja_gurung@softprodigy.com>
2023-04-04 11:11:16 +0530

    worked on exportong issue

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,5 +1,35 @@
 import { NextSeo } from "next-seo";
-import { getToken,getPreview } from "../../utils/callApi";
+
+
+export const getToken = async () => {
+  const tokenRes = await fetch(
+    "https://drwhm1ofhh.execute-api.ap-south-1.amazonaws.com/qa/api/v1/auth/guest",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        mode: "no-cors",
+        ip: "1696313264460",
+        os: "Windows",
+        version: "1.1",
+      }),
+    }
+  );
+  const jsonToken = await tokenRes.json();
+  const finalToken = await jsonToken?.data?.guestToken;
+  return finalToken
+}
+
+export const getPreview = async (id, finalToken) => {
+  const res = await fetch(
+    `https://3mxtu0s5yl.execute-api.ap-south-1.amazonaws.com/qa/api/v1/web/hoote/${id}`,
+    {
+      method: "GET",
+      headers: { Authorization: `Bearer ${finalToken}` },
+    }
+  );
+  return res;
+};
 
 const Blog = ({ resData }) => {
   const imgUrl =  resData.hoote.files[0].thumbnail
